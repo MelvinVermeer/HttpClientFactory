60b5dbd254aead37aa9b1b74da6a08846fbd559f
Melvin Vermeer <melvin.vermeer@gmail.com>
2018-06-29 22:23:19 +0200

    implenet using : Instantiate HttpClient

--- ResilientHttpClient/Pages/Index.cshtml.cs
+++ ResilientHttpClient/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,9 +10,16 @@ namespace ResilientHttpClient.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public IEnumerable<User> Users { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            var apiUrl = "https://jsonplaceholder.typicode.com/users";
+            var client = new HttpClient();
+            var response = await client.GetAsync(apiUrl);
+            response.EnsureSuccessStatusCode();
+            
+            Users = await response.Content.ReadAsAsync<IEnumerable<User>>();
         }
     }
 }
